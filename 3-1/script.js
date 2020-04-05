06ecdea6ac89af2e7a83d025d8bf8beb24f9f309
novasagittarii (thoma) <novasagittarii@gmail.com>
2020-04-04 21:24:47 -0700

    i did it yay

--- 3-1/script.js
+++ 3-1/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 	var checkInt = setInterval(function() {
-		var perc = Number($('#progress-bar').attr('aria-valuenow'));
+		var perc = Number($('.progress-bar').attr('aria-valuenow'));
 		perc += (100-perc)/2;
 		if (perc > 99.99) {
 			clearInterval(checkInt);
-			$('a.alert').show();
+			$('.alert').show();
 			return;
 		}
+		$('.progress-bar').attr('aria-valuenow', ''+perc);
 		$('.progress-bar').css('width', perc+'%');
 	}, 750);
 });
